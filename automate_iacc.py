20755dcf6f18fccf680daa6f899cf8d991a262a5
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2021-02-13 02:57:52 +0100

    Overhauled

--- automate_iacc.py
+++ automate_iacc.py
@@ -2,39 +2,76 @@ import gspread
 import oauth2client
 import csv
 import string
-import time
+import pprint
 
 gc = gspread.oauth()
 
 semester = 'Fall 20'
-board_members = ('Leila Farah Moussa', 'Hanane Nour Moussa', 'Yassir Benabdallah', 'Naeem Nisar Sheikh')
+board_members = ('Leila Farah Moussa') # And co.
 
 def removeBadBytes(filename):
     # This is some weird encoding issue I don't know how to fix.
     fi = open(filename, 'rb')
     data = fi.read()
     fi.close()
-    fo = open('mynew.csv', 'wb')  # Temp file.
+    fo = open('mynew.csv', 'wb')
     fo.write(data.replace(b'\x00', b''))
     fo.close()
     f = open('mynew.csv')
     r = csv.DictReader((line.replace('ÿþ','') for line in f), delimiter='\t')
     return r
 
-def getRangeEnd(first_empty_row, number_rows, date_column) -> str:
-    """
-    Returns the last cell we're writing to in A1 notation
-    """
-    col = string.ascii_uppercase[date_column-1]
-    row = first_empty_row + number_rows - 1
-    cell = col + str(row)
-    return cell
+def get_first_empty_row(recs: list) -> int:
+    for i, row in enumerate(recs):
+        if row['Name'] == '':
+            return i
 
-def getFirstEmptyRow(worksheet) -> int:
-    row = 2
-    while worksheet.cell(row, 1) != '':
-        row += 1
-    return row
+def get_csv_data(reader) -> tuple:
+    attendees = set()
+    for i, data_row in enumerate(reader):
+        # Encoding problems with Excel files turned to CSV!
+        if i == 0:
+            date = data_row['Timestamp'].split(',')[0]
+            date = date[:-2]
+        attendees.add(data_row['Full Name'])
+    return attendees, date
+
+def find_name(recs: list, name: str) -> int:
+    for i, row in enumerate(recs):
+        if row['Name'] == name:
+            return i
+    return -1
+
+def populate_records(attendees: set, recs: list, date: str) -> None:
+    first_empty = get_first_empty_row(recs)
+    if first_empty is None:
+        print('You need more empty rows.')
+    
+    for person in attendees:
+        split = person.split('<')
+        if len(split) == 1:
+            name, _id = person, ''
+        elif len(split) == 2:
+            name, _id = split[0].strip(), split[1].strip()[:5]
+        name = name.title()
+
+        index = find_name(recs, name)
+        if index < 0:
+            # First time attending.
+            recs[first_empty]['Name'] = name
+            recs[first_empty]['ID'] = _id
+            recs[first_empty][date] = 1
+            first_empty += 1
+        else:
+            recs[index][date] = 1
+            recs[index]['ID'] = _id
+
+def write_back(recs: list, worksheet) -> None:
+    number_rows = len(recs)
+    lists = []
+    for elt in recs:
+        lists.append(list(elt.values()))
+    worksheet.update(f'A2:{number_rows+1}', lists)
 
 def update_meetings():
     print("Okay!")
@@ -42,50 +79,30 @@ def update_meetings():
     sh = gc.open("Attendance Records")
     worksheet = sh.worksheet(semester)
 
-    #filename = input("Attendance list path?\n")
-    filename = 'sheet.csv'
-    reader = removeBadBytes(filename)
-
-    attendees = set()
-    for i, data_row in enumerate(reader):
-        # Very bad encoding problems with Excel files turned to CSV!
-        if i == 0:
-            date = data_row['Timestamp'].split(',')[0]
-            date = date[:-2]  # Just for formatting, remove '20'.
-        attendees.add(data_row['Full Name'])
-
-    date_column = worksheet.find(date, 1, None).col
-    # All dates are already there.
+    recs = worksheet.get_all_records()
     
-    missing = []
-    existing = 0
-    for person in attendees:
-        split = person.split('<')
-        if len(split) == 1:
-            name, _id = person, ''
-        elif len(split) == 2:
-            name, _id = split[0], split[1][:5]
-        name = name.title()  # Capitalize names.
+    del recs[-1]
+    for row in recs:
+        del row['Count of attendance so far']
 
-        try:
-            name_row = worksheet.find(name, None, 1).row
-            print("Name already exists")
-            existing += 1
-            worksheet.update_cell(name_row, date_column, 1)
-        except:
-            print("Adding new name...")
-            missing.append([name, _id]+[0]*(date_column-3)+[1])
+    #pp = pprint.PrettyPrinter()
+    #pp.pprint(recs)
 
-    print("Waiting for a minute to avoid exhausting the resource. Go drink some water or something.")
-    time.sleep(60)
-    first_empty_row = getFirstEmptyRow(worksheet)
+    filename = input("Attendance list path?\n")
+    # filename = 'sheet.csv'
+    reader = removeBadBytes(filename)
+    attendees, date = get_csv_data(reader)
 
-    last_cell = getRangeEnd(first_empty_row, len(missing), date_column)
-    worksheet.update(f'A{first_empty_row}:{last_cell}', missing)
+    populate_records(attendees, recs, date)
+
+    write_back(recs, worksheet)
 
     print("All done!")
     return
+
     ########################################
+    # May or may not come back here...
+    
     print("The 3 most active non-board members...")
     count_column = worksheet.find('Count of attendance so far', 1, None).col
     counts = worksheet.col_values(count_column)  # This is an ordered list.
@@ -101,7 +118,7 @@ def update_meetings():
     
     print("The most popular monday meeting so far...")
     # find single max value in last row
-    event_row = worksheet.find('Event Attendance', None, None) or 44  # resolves to boolean or actual choices?
+    event_row = worksheet.find('Event Attendance', None, None) or 44  # Should be length of records!
     events = worksheet.row_values(event_row)
     print("events row", events)
     max_att = 0
@@ -118,6 +135,7 @@ def update_tutoring():
 
 if __name__ == '__main__':
     event = input('"M" for monday meeting, "T" for Dar Al Aman:\n')
+    # event = 'M'
     if event == 'M':
         update_meetings()
     elif event == 'T':
